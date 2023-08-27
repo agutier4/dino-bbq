c68d7052b1498bf339159fe5fb02c73869ccef31
Andrew Gutierrez <agut@comcast.net>
2023-08-26 21:56:13 -0400

    hook up MQTT and WebSockets interfaces

---
+++ server/public/js/socket.js
@@ -0,0 +1,37 @@
+function connectSocket(endpoint){
+    if (socket !== undefined) {
+        socket.close()
+    }
+
+    socket = new WebSocket(endpoint);
+
+    socket.onmessage = function(event) {
+        console.log("Message:" + event.data);
+    }
+
+    socket.onopen = function(evt) {
+        console.log("WebSocket opened");
+    };
+
+    socket.onclose = function(evt) {
+        console.log("WebSocket closed");
+    };
+
+    socket.onerror = function(evt) {
+        console.log("WebSocket Error");
+    };
+
+    return socket;
+}
+
+function sendMsg() {
+    var message = document.getElementById("myMessage").value;
+    socket.send(message);
+}
+
+function closeSocket() {
+    socket.close();
+}
+
+const socketUrl = 'ws://'+location.host+'/cmd'
+var socket = connectSocket(socketUrl);
--- server/server.js
+++ server/server.js
@@ -1,12 +1,66 @@
+const pino = require('pino')
+const logger = pino({
+  transport: {
+    target: 'pino-pretty'
+  },
+})
+
+const ascii = String.raw`
+  _____  _               ____  ____   ____  
+ |  __ \(_)             |  _ \|  _ \ / __ \ 
+ | |  | |_ _ __   ___   | |_) | |_) | |  | |
+ | |  | | | '_ \ / _ \  |  _ <|  _ <| |  | |
+ | |__| | | | | | (_) | | |_) | |_) | |__| |
+ |_____/|_|_| |_|\___/  |____/|____/ \___\_\                                           
+
+`
+
+var nodes = []
+logger.info(ascii)
+logger.info('Initializing Dino server...');
+
+//=============================================
+//             Start MQTT Broker
+//=============================================
+const aedes = require('aedes')()
+const broker = require('net').createServer(aedes.handle)
+const port = 1883
+
+broker.listen(port, function () {
+  logger.info('[MQTT] Server started and listening on port %s', port);
+})
+
+aedes.on('client', function (client) {
+    logger.info('[MQTT] Client Connected: %s',client.id);
+})
+
+aedes.on('clientDisconnect', function (client) {
+    logger.info('[MQTT] Client Disconnected: %s',client.id);
+})
+
+aedes.on('subscribe', function (subscriptions, client) {
+    logger.info('[MQTT] Client %s subscribed to topics: %s', client.id, subscriptions.map(s => s.topic).join('\n'));
+})
+
+//=============================================
+//             MQTT Subscriptions
+//=============================================
+aedes.subscribe('nodes',function(packet, cb){
+    // TODO: maintain nodes
+    nodes = JSON.parse(packet.payload.toString())
+    logger.info('[MQTT] Received node update: %s', nodes);
+})
+
+//=============================================
+//           HTTP Endpoint Mappings
+//=============================================
 const express = require('express')
 const app = express()
-const fs = require('fs');
+const fs = require('fs')
+var expressWs = require('express-ws')(app);
 
 app.use(express.static('public'))
 
-//=============================================
-//             Endpoint mappings
-//=============================================
 app.get('/', function (req, res) {
     fs.readFile('public/html/index.html',function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
@@ -17,14 +71,36 @@ app.get('/', function (req, res) {
 
 // roar API endpoint 
 app.get('/api/roar', function (req, res) {
-    console.log('ROAR');
+    logger.info('[HTTP] Roar API request');
+    aedes.publish({topic:'cmd', payload:'{"id":255}'}, null);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('SUCCESSS');
+    res.end();
+});
+
+app.ws('/cmd', function(ws, req) {
+    logger.info('[WS] Client connected');
+    ws.send(JSON.stringify(nodes));
+
+    ws.on('message', function(msg) {
+        var cmdJson = '{"id":' + parseInt(msg) + '}';
+
+        logger.info('[WS] Command Received: %s', cmdJson);
+        aedes.publish({topic:'cmd', payload:cmdJson}, null);
+        ws.send(cmdJson);
+    });
+
+    ws.on('close', function(ws, req){
+        ws.address
+        logger.info('[WS] Client disconnected');
+    });
 });
 
 //=============================================
-//                Start Server
+//             Start HTTP Server
 //=============================================
 var server = app.listen(3000, function () {
     var port = server.address().port;
-    console.log('Initializing SPRUCE server...');
-    console.log('Server started at http://localhost:%s/', port);
+    logger.info('[HTTP] Server started at http://localhost:%s/', port);
 });
